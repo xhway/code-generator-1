998f4b9c3c4648fa1436f1f722afe52ab5f21bd3
Xhway <dhway20@gmail.com>
2022-12-22 23:27:37 -0500

    Final code

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,14 @@
-// Assignment Code
 
-
-
-//Scope
+//Variables for password and password generator
 var password = "";
 
 var generatePassword = function(){
   password = "";
 
-//
+//Variables for length of password
 var lengthPassword = passwordLength();
 
-//
+
 var passwordInc = characterInc();
 var passwordIncLength = passwordInc.length;
 
@@ -20,17 +17,17 @@ for (var i = 0; i < lengthPassword; i++){
 }
 return password;
 };
-
+//Prompt to ask length of password
 var passwordLength = function(){
-  var lengthPassword = prompt("Enter length of password. Password must be between 8 and 128 characters.");
-  if (lengthPassword < 8 || lengthPassword > 128 || lengthPassword === ""){
-    return passwordLength;
+  var passwordLength = prompt("Enter length of password. Password must be between 8 and 128 characters.");
+  if (passwordLength < 8, passwordLength > 128){
+    passwordLength = Math.floor(Number(passwordLength));
+  }return passwordLength;
   }
-lengthPassword = Math.floor(Number(lengthPassword));
 
-return passwordLength;
 
-}
+
+
 //
 
 var characterInc = function (){
@@ -42,7 +39,8 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specChar = "!@#$%^&*()-_+=<>";
-//Confirmation
+//Confirmation for character sets
+
 var confirmLowerCase = confirm("Must have one lower case letter.");
 var confirmUpperCase = confirm("Must contain one upper case letter.");
 var confirmNumber = confirm("Must contain a number.");
